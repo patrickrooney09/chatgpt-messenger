7eeeee0a07359f4e8f2ea6320b44b94439a16876
Patrick Rooney <patrickrooney09@gmail.com>
2023-06-30 16:39:19 -0400

    sidebar skeleton built

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,5 +1,19 @@
+import NewChat from "./NewChat";
+
 function SideBar() {
-  return <div>sidebar goes here</div>;
+  return (
+    <div className="p-2 flex flex-col h-screen">
+      <div className="flex-1">
+        <div>
+          {/* New Chat */}
+          <NewChat />
+          <div>{/*Model Selection*/}</div>
+
+          {/*Map Through CharRows*/}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SideBar;
